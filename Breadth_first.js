0234cbfc7cddd287daf54cdcd9958971898ade51
4lokym <mykola.koshilko@gmail.com>
2025-06-12 10:01:00 +0200

    Breadth first alg done

--- Breadth_first.js
+++ Breadth_first.js
@@ -1,20 +1,45 @@
-class MyNode{
-  constructor(content, nearNodes){
+class MyNode {
+  constructor(content, nearNodes) {
     this.content = content;
     this.nearNodes = nearNodes;
   }
 
-  addNode(node){
+  addNode(node) {
     this.nearNodes.push(node);
   }
 
-  static connect(node1, node2){
+  static connect(node1, node2) {
     node1.addNode(node2);
     node2.addNode(node1);
   }
+
+  static visitBreadthFirst(root, callback = () => {}) {
+    //this array remembers what node has been visited
+    const nuovo = {};
+    const queue = [];
+    const graph = [];
+    queue.push(root);
+
+    while (queue.length !== 0) {
+      let temp = queue.shift();
+      callback(temp);
+      graph.push(temp);
+
+      nuovo[temp.content] = 1;
+
+      for (let t of temp.nearNodes) {
+        if (nuovo[t.content]) {
+          continue
+        } else {
+          queue.push(t);
+          nuovo[t.content] = 1;
+        }
+      }
+    }
+    return graph;
+  }
 }
 
-const graph = [];
 const s = new MyNode(`s`, []);
 const a = new MyNode(`a`, []);
 const b = new MyNode(`b`, []);
@@ -36,5 +61,10 @@ MyNode.connect(e, g);
 MyNode.connect(g, f);
 MyNode.connect(g, h);
 
+const graph = MyNode.visitBreadthFirst(s);
 
+const n = performance.now();
+console.log(graph);
+const n1 = performance.now();
 
+console.log(`${n1- n}ms`)
